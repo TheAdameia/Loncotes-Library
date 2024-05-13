63c64f85f8e452fe9b5e86c849a8b05d1c250426
Adameia Ramsey <adameiaproductions@gmail.com>
2024-05-13 09:54:09 -0500

    several more endpoints done, previous ones adjusted to more fit proj reqs

--- Program.cs
+++ Program.cs
@@ -56,23 +56,59 @@ app.MapGet("/api/materials", (LoncotesLibraryDbContext db, int? GenreInput, int?
     }).ToList();
 });
 
+// this was a good first attempt but it was not correct
+// app.MapGet("/api/materials/{id}", (LoncotesLibraryDbContext db, int id) =>
+// {
+//     // instead of "joining" checkouts, I could create a new list for them here and then first-search it for the right one.
+
+//     List<CheckoutDTO> checkoutList = db.Checkouts.Select(c => new CheckoutDTO
+//     {
+//         Id = c.Id,
+//         MaterialId = c.MaterialId,
+//         PatronId = c.PatronId,
+//         CheckoutDate = c.CheckoutDate
+//     }).ToList();
+
+//     List<CheckoutDTO> correctCheckouts = checkoutList.Where(c => c.MaterialId == id).ToList();
+
+//     return db.Materials
+//     .Include(m => m.Genre)
+//     .Include(m => m.MaterialType)
+//     .Select(m => new MaterialDTO
+//     {
+//         Id = m.Id,
+//         MaterialName = m.MaterialName,
+//         MaterialTypeId = m.MaterialTypeId,
+//         MaterialType =  new MaterialTypeDTO
+//         {
+//             Id = m.MaterialType.Id,
+//             Name = m.MaterialType.Name,
+//             CheckoutDays = m.MaterialType.CheckoutDays
+//         },
+//         GenreId = m.GenreId,
+//         Genre = new GenreDTO
+//         {
+//             Id = m.Genre.Id,
+//             Name = m.Genre.Name
+//         },
+//         Checkouts = correctCheckouts.Select(c => new CheckoutDTO
+//         {
+//             Id = c.Id,
+//             MaterialId = c.MaterialId,
+//             PatronId = c.PatronId,
+//             CheckoutDate = c.CheckoutDate
+//         }).ToList()
+//     });
+
+// });
+
 app.MapGet("/api/materials/{id}", (LoncotesLibraryDbContext db, int id) =>
 {
-    // instead of "joining" checkouts, I could create a new list for them here and then first-search it for the right one.
-
-    List<CheckoutDTO> checkoutList = db.Checkouts.Select(c => new CheckoutDTO
-    {
-        Id = c.Id,
-        MaterialId = c.MaterialId,
-        PatronId = c.PatronId,
-        CheckoutDate = c.CheckoutDate
-    }).ToList();
-
-    List<CheckoutDTO> correctCheckouts = checkoutList.Where(c => c.MaterialId == id).ToList();
-
     return db.Materials
     .Include(m => m.Genre)
     .Include(m => m.MaterialType)
+    .Include(m => m.Checkouts)
+        .ThenInclude(co => co.Patron)
     .Select(m => new MaterialDTO
     {
         Id = m.Id,
@@ -89,20 +125,40 @@ app.MapGet("/api/materials/{id}", (LoncotesLibraryDbContext db, int id) =>
         {
             Id = m.Genre.Id,
             Name = m.Genre.Name
-        },
-        Checkouts = correctCheckouts.Select(c => new CheckoutDTO
-        {
-            Id = c.Id,
-            MaterialId = c.MaterialId,
-            PatronId = c.PatronId,
-            CheckoutDate = c.CheckoutDate
-        }).ToList()
+        }
     });
 
 });
 
+app.MapPost("/api/materials", (LoncotesLibraryDbContext db, Material material) =>
+{
+    db.Materials.Add(material);
+    db.SaveChanges();
+    return Results.Created($"/api/materials/{material.Id}", material);
+});
 
+app.MapDelete("/api/materials/{id}", (LoncotesLibraryDbContext db, int id) =>
+{
+    Material material = db.Materials.SingleOrDefault(m => m.Id == id);
+    if (material == null)
+    {
+        return Results.NotFound();
+    }
+    material.OutOfCirculationSince = DateTime.Now;
+    db.SaveChanges();
+    return Results.NoContent();
+});
 
+app.MapGet("/api/materialTypes", (LoncotesLibraryDbContext db) =>
+{
+    return db.MaterialTypes
+    .Select(m => new MaterialTypeDTO
+    {
+        Id = m.Id,
+        Name = m.Name,
+        CheckoutDays = m.CheckoutDays
+    });
+});
 
 app.Run();
 
